eb0a87574f396fc76e41a0eb1aa76e77acb853e0
Mrchen <972747934@qq.com>
2019-07-14 18:09:46 +0800

    git commit

---
+++ GitTest/src/com/mrchen/mainTest.java
@@ -0,0 +1,14 @@
+package com.mrchen;
+
+public class mainTest {
+    public static void main(String[] args) {
+        System.out.println("git for test");
+        System.out.println("git for test");
+        System.out.println("git for test");
+        System.out.println("git for test");
+    }
+
+    public void printArray(){
+        System.out.println("array");
+    }
+}
